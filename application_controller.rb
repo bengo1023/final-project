12912dc7eefb04c05e48c96f99061c5c069dc857
18benjamin <18benjamin@friendsseminary.org>
2016-08-18 20:19:14 +0000

    all services

--- application_controller.rb
+++ application_controller.rb
@@ -11,8 +11,16 @@ class ApplicationController < Sinatra::Base
 #     binding.pry
     @t = params[:time].to_f
     @d = params[:distance].to_f
-    @via_fare="Via fare: $#{via_fare_calculator(@t,@d,params[:day],params[:traffic])}"
-    @taxi_fare="Taxi fare: $#{nyc_taxi_calculator(@t,@d,params[:day],params[:traffic])}"
+    @via_fare="Via fare: $#{via_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @taxi_fare="Taxi fare: $#{nyc_taxi_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @uberX_fare="UberX fare: $#{uberX_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @uberXL_fare="UberXL fare: $#{uberXL_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @uberBlack_fare="UberBlack fare: $#{uberBlack_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @uberSUV_fare="UberSUV fare: $#{uberSUV_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @lyft_standard="Lyft Standard fare: $#{lyft_standard_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @lyft_plus="Lyft Plus fare: $#{lyft_plus_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @lyft_premier="Lyft Premier fare: $#{lyft_premier_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
+    @lyft_line="Lyft Line fare: $#{lyft_line_fare_calculator(@t,@d,params[:day],params[:traffic]).round(2)}"
     erb :results
   end
 
